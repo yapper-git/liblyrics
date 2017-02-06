e9f180147442df680ec8da114b23382e3ec7aab6
Yvan Duron <yvan.duron@gmail.com>
2017-02-06 22:41:51 +0100

    Ajout du caractère allemand ß

--- liblyrics/common.py
+++ liblyrics/common.py
@@ -6,6 +6,7 @@ COMMENT_SIGN, OPEN_HEAD, CLOSE_HEAD, EOL \
         = map(Suppress, "#[]\n")
 allowed_chars = printables + " "
 allowed_chars += " …’«»–—àâéèêëïîôùûçœÀÂÉÈÊËÏÎÔÙÛÇŒ"  # French chars
+allowed_chars += "ß"  # German chars
 allowed_chars += "{}×23"  # repeat chars
 WORDS   = Word(allowed_chars)
 INTEGER = Word(nums)
