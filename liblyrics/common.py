d81b7c4aca3fd545802f9d2dcff317c673f1b66b
Yvan Duron <yvan.duron@gmail.com>
2017-02-12 22:20:41 +0100

    Amélioration de la lisibilité des caractères autorisés

--- liblyrics/common.py
+++ liblyrics/common.py
@@ -5,7 +5,9 @@ ParserElement.setDefaultWhitespaceChars("")
 COMMENT_SIGN, OPEN_HEAD, CLOSE_HEAD, EOL \
         = map(Suppress, "#[]\n")
 allowed_chars = printables + " "
-allowed_chars += " …’‘«»“”–—àâäéèêëïîôöùûüçœÀÂÄÉÈÊËÏÎÔÖÙÛÜÇŒß"  # French and German chars
+allowed_chars += " …’‘«»“”–—ß
+allowed_chars += "àâäéèêëïîôöùûüçœ"
+allowed_chars += "ÀÂÄÉÈÊËÏÎÔÖÙÛÜÇŒ"
 allowed_chars += "{}×23"  # repeat chars
 WORDS   = Word(allowed_chars)
 INTEGER = Word(nums)
